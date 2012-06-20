5b14f8657613ec6525200ec5eacb8f6e97d12292
Gordon McAllister <mail.gordon.mcallister@gmail.com>
2012-06-20 15:39:25 +0200

    FEATURE: Validating new questions before entry  * Catching the change of input for the textarea  * Validating that there is a question mark at the end of the question  * Displaying errors on the page  * Validating that the new question has a minimum of 3 words  * Create button is disabled from start

--- public/scripts/apolloh.js
+++ public/scripts/apolloh.js
@@ -12,5 +12,57 @@ var Apolloh = (function($) {
     });
 
     nudgeArticle();
-  });    
+  });   
+
+  checkForQuestionMark = function (text) {
+    if (text.trim().slice(-1) !== "?") {
+      return "Please put a question mark at the end.";
+    }
+    return null;
+  };
+
+  checkWordCount = function (text, count) {
+    oneLine = text.replace(/[\s|?]/g,' ');
+    words = oneLine.trim().split(' ');
+    if (words.length < count) {
+      return "Please enter at least 3 words.";
+    }
+    return null;    
+  };
+
+  $(function() {
+    $('#question-text').bind('keypress paste input', function(){
+      $('#errors').remove();
+
+      var text = $(this).val();
+
+      var validations = [
+        {routine: checkWordCount, args:[text, 3]},
+        {routine: checkForQuestionMark, args:[text]}
+      ];
+
+      // validation the text
+      var messages = [];
+      $.each(validations, function(index, value){
+        var message = value.routine.apply(null, value.args);
+        if (message)
+          messages.push(message);       
+      });
+
+      // do we have any errors?
+      if (messages.length === 0) {
+        $('#create').removeAttr('disabled');
+      } else {
+        $('<div id="errors"></div>').insertBefore('#create');
+        $.each(messages, function(index, value) {
+          $('#errors').append('<p class="warning">'+value+'</p>');
+        });
+        $('#create').attr('disabled','disabled');
+      }
+      
+    });
+  });   
+
+
+ 
 })(jQuery);
